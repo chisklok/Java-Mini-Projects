9f97f70d0f2f5573739fe5fc6b03fc5b985328ac
chisklok <cilok7442@gmail.com>
2025-11-22 06:11:45 +0700

    Update Note.java

--- note-cli/src/model/Note.java
+++ note-cli/src/model/Note.java
@@ -1,1 +1,23 @@
+package model;
+
+public class Note {
+    private String id;
+    private String title;
+    private String body;
+    private String created;
+
+    public Note(String id, String title, String body, String created) {
+        this.id = id; this.title = title; this.body = body; this.created = created;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getBody() { return body; }
+    public String getCreated() { return created; }
+
+    @Override
+    public String toString() {
+        return id + " | " + title + " | " + created;
+    }
+}
 
