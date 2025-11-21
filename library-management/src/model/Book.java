7e3362529a3eb7bee6863673da2ca22e8c31aa83
chisklok <cilok7442@gmail.com>
2025-11-22 05:56:34 +0700

    Update Book.java

--- library-management/src/model/Book.java
+++ library-management/src/model/Book.java
@@ -1,1 +1,24 @@
+package model;
+
+public class Book {
+    private String title;
+    private String author;
+    private String id;
+    private boolean borrowed;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.borrowed = false;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isBorrowed() { return borrowed; }
+
+    public void borrow() { this.borrowed = true; }
+    public void returned() { this.borrowed = false; }
+}
 
