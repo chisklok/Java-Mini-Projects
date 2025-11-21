cc0a6813de192517c4cbbbbc66c84f531c6d5a73
chisklok <cilok7442@gmail.com>
2025-11-22 06:02:04 +0700

    Update Student.java

--- student-grades/model/Student.java
+++ student-grades/model/Student.java
@@ -1,1 +1,27 @@
+package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String id;
+    private String name;
+    private List<Grade> grades = new ArrayList<>();
+
+    public Student(String id, String name) { this.id = id; this.name = name; }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public List<Grade> getGrades() { return grades; }
+
+    public void addGrade(Grade g) { grades.add(g); }
+
+    public double average() {
+        return grades.stream().mapToDouble(Grade::getScore).average().orElse(0.0);
+    }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | avg: " + String.format("%.2f", average());
+    }
+}
