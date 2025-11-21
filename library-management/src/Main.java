0e2c40898975cbe23a4e54e3b64ed663ef83affe
chisklok <cilok7442@gmail.com>
2025-11-22 05:57:27 +0700

    Update Main.java

--- library-management/src/Main.java
+++ library-management/src/Main.java
@@ -1,1 +1,42 @@
+import model.Book;
+import service.LibraryService;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        LibraryService lib = new LibraryService();
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n1. Add Book\n2. Search\n3. Borrow\n4. Exit");
+            String c = in.nextLine();
+
+            switch (c) {
+                case "1":
+                    System.out.print("ID: ");
+                    String id = in.nextLine();
+                    System.out.print("Title: ");
+                    String title = in.nextLine();
+                    System.out.print("Author: ");
+                    String author = in.nextLine();
+                    lib.addBook(new Book(id, title, author));
+                    break;
+
+                case "2":
+                    System.out.print("Keyword: ");
+                    String k = in.nextLine();
+                    lib.search(k).forEach(b -> 
+                        System.out.println(b.getId() + " | " + b.getTitle() + " - " + b.getAuthor())
+                    );
+                    break;
+
+                case "3":
+                    System.out.print("Book ID: ");
+                    lib.borrow(in.nextLine());
+                    break;
+
+                default: return;
+            }
+        }
+    }
+}
