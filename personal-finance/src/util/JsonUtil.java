e3074e52af0a9631d748471c5036f6a842c249c1
chisklok <cilok7442@gmail.com>
2025-11-22 06:06:46 +0700

    Update JsonUtil.java

--- personal-finance/src/util/JsonUtil.java
+++ personal-finance/src/util/JsonUtil.java
@@ -1,1 +1,32 @@
+package util;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import model.Transaction;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtil {
+    private static final String FILE = "transactions.json";
+    private static final Gson gson = new Gson();
+
+    public static List<Transaction> loadTransactions() {
+        try (Reader r = new FileReader(FILE)) {
+            Type tt = new TypeToken<List<Transaction>>(){}.getType();
+            List<Transaction> list = gson.fromJson(r, tt);
+            return list != null ? list : new ArrayList<>();
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void saveTransactions(List<Transaction> list) {
+        try (Writer w = new FileWriter(FILE)) {
+            gson.toJson(list, w);
+        } catch (IOException ignored) {}
+    }
+}
 
