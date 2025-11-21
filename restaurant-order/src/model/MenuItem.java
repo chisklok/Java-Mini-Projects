37c6f9479bbb059553e000022c84bdeb35bd0c0f
chisklok <cilok7442@gmail.com>
2025-11-22 06:09:06 +0700

    Update MenuItem.java

--- restaurant-order/src/model/MenuItem.java
+++ restaurant-order/src/model/MenuItem.java
@@ -1,1 +1,21 @@
+package model;
+
+public class MenuItem {
+    private String code;
+    private String name;
+    private double price;
+
+    public MenuItem(String code, String name, double price) {
+        this.code = code; this.name = name; this.price = price;
+    }
+
+    public String getCode() { return code; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return code + " | " + name + " | Rp " + price;
+    }
+}
 
