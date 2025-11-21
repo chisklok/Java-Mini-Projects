8cf4700b0a3e6efe536407fb35e480591ddda00a
chisklok <cilok7442@gmail.com>
2025-11-22 05:56:53 +0700

    Update LibraryService.java

--- library-management/src/service/LibraryService.java
+++ library-management/src/service/LibraryService.java
@@ -1,1 +1,42 @@
 
+package service;
+
+import model.Book;
+import util.JsonUtil;
+import java.util.*;
+
+public class LibraryService {
+    private List<Book> books;
+
+    public LibraryService() {
+        books = JsonUtil.loadBooks();
+    }
+
+    public void addBook(Book b) {
+        books.add(b);
+        JsonUtil.saveBooks(books);
+    }
+
+    public List<Book> search(String keyword) {
+        keyword = keyword.toLowerCase();
+        List<Book> results = new ArrayList<>();
+
+        for (Book b : books) {
+            if (b.getTitle().toLowerCase().contains(keyword) ||
+                b.getAuthor().toLowerCase().contains(keyword)) {
+                results.add(b);
+            }
+        }
+        return results;
+    }
+
+    public void borrow(String id) {
+        for (Book b : books) {
+            if (b.getId().equals(id) && !b.isBorrowed()) {
+                b.borrow();
+                JsonUtil.saveBooks(books);
+                return;
+            }
+        }
+    }
+}
